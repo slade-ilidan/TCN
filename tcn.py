d2fc8e43f2fbaa247fbc0174a3109f4f96f2dbe1
slade-ilidan <sladeilidan@proton.me>
2025-05-19 00:36:43 -0500

    added skip connections

--- tcn.py
+++ tcn.py
@@ -10,23 +10,29 @@ def tcn_block(in_channels, out_channels, kernel, dilation, dropout):
     x = keras.layers.LayerNormalization()(x)
     x = keras.layers.ReLU()(x)
     x = keras.layers.SpatialDropout1D(dropout)(x)
+    x_skip = x
 
     if in_channels.shape[-1] != out_channels: in_channels = keras.layers.Conv1D(out_channels, kernel_size=1)(in_channels)
     x = keras.layers.Add()([x, in_channels])
+    x = keras.layers.ReLU()(x)
+    return x, x_skip
 
-    return x
-
-def tcn(num_inputs, channels_list=[64], kernel=3, dropout=0.2, repeat=1):
+def tcn(num_inputs, channels_list=[64], kernel=3, dropout=0.2, repeat=1, skip=True):
     inputs = keras.layers.Input(shape=num_inputs)
     x = inputs
 
     levels = len(channels_list)
+    skip_connections = []
     for level, channels in enumerate(channels_list):
         dilation = 2**level
         level_input = x
 
         for _ in range(repeat):
-            x = tcn_block(x, channels, kernel, dilation, dropout)
+            x, x_skip = tcn_block(x, channels, kernel, dilation, dropout)
+            skip_connections.append(x_skip)
+
+    if skip and skip_connections:
+        x = keras.layers.Add()(skip_connections)
 
     x = keras.layers.GlobalAveragePooling1D()(x)
     output = keras.layers.Dense(1, activation='sigmoid')(x)
